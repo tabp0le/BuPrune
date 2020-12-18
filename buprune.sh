64240aff5df165b34b822a590d63c5de29add5a9
Tab Fitts <tfitts1212@gmail.com>
2020-12-17 20:07:59 -0700

    Add initial pruning script

---
+++ buprune.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#############################################
+############# Clean Shit Up #################
+############## by tabp0le ###################
+#############################################
+#############################################
+
+### Settings ###
+
+logfile="/tmp/prune.log";
+prunepath=""
+days=30
+recursive=1
+dryrun=1
+
+### End Settings ###
+
+recurse="$prunepath"
+norecurse="$prunepath -maxdepth 1"
+
+if [ $recursive -eq "1" ]
+  then
+    export pp=$recurse
+  else
+    export pp=$norecurse
+fi
+
+echo 'Begin scheduled pruning' >> $logfile
+echo $(date) >> $logfile
+echo 'Deleting the following files:' >> $logfile
+find $pp -mtime +$days -type f >> $logfile
+
+if [ $dryrun -eq "1" ]
+  then
+    echo 'DRY RUN' >> $logfile
+  else
+    find $pp -mtime +30 -type f -delete >> $logfile
+fi
+
+echo 'Scheduled pruning complete' >> $logfile
+echo $(date) >> $logfile
